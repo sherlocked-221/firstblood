85093a33fedc534f450740d951decc3e94818180
sherlocked-221 <1928249714@qq.com>
2025-03-17 13:34:20 +0800

    汉诺塔问题

---
+++ Hanoi.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void hanoi(int n,char x,char y,char z){
+	if(n==1){
+		printf("%c-->%c\n",x,z);
+	}else{
+		hanoi(n-1,x,z,y);
+		printf("%c-->%c\n",x,z);
+		hanoi(n-1,y,x,z);
+	}
+}
+int main()
+{
+	int n;
+	printf("ÇëÊäÈëººÅµËşµÄ²ãÊı£º");
+	scanf("%d",&n);
+	hanoi(n,'X','Y','Z');
+	return 0;
+}
