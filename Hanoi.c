4c1932c74f5300fe91e50b677b4e09cf3ef4297c
sherlocked-221 <1928249714@qq.com>
2025-03-27 21:27:28 +0800

    更新汉诺塔程序，修改提示信息为中文

--- Hanoi.c
+++ Hanoi.c
@@ -11,7 +11,7 @@ void hanoi(int n,char x,char y,char z){
 int main()
 {
 	int n;
-	printf("ÇëÊäÈëººÅµËşµÄ²ãÊı:");
+	printf("è¯·è¾“å…¥æ±‰è¯ºå¡”çš„å±‚æ•°:");
 	scanf("%d",&n);
 	hanoi(n,'X','Y','Z');
 	return 0;
